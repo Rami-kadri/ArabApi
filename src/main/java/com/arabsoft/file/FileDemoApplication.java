7ab69681f6d9b4756baaec237ffab6cb7dc2ebc4
Rami Abdelkader <abdelkader.rami1996@gmail.com>
2022-08-08 11:26:02 +0100

    upload/donload file

---
+++ src/main/java/com/arabsoft/file/FileDemoApplication.java
@@ -0,0 +1,18 @@
+package com.arabsoft.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.arabsoft.file.property.FileStorageProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class FileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDemoApplication.class, args);
+	}
+}
